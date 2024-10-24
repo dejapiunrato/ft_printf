b2d915ee1849819c1c846ad4771bc51caac46073
dejapiunrato <andersh.pics@gmail.com>
2024-10-24 18:59:35 +0200

    start

---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: piesito <piesito@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 20:45:02 by piesito           #+#    #+#             */
+/*   Updated: 2024/10/21 20:58:51 by piesito          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_C
+# define FT_PRINTF_H
+
+#include "libft/libft.h"
+#include <stdarg.h>
+
+void	ft_printf(const char *str, ...);
+
+#endif
